a90a4635858ab0d3ff30e9f26c2d1475b22bd20b
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-01-11 23:53:15 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,31 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+      
+        @knowledge << string
+    end
+
+    def knowledge 
+        @knowledge 
+    end
 
 end
+
+# Student
+#   inherits from the User class
+#   #first_name
+#     has a first name
+#   #last_name
+#     has a last name
+#   #new
+#     initializes with an empty knowledge array
+#   #learn
+#     takes in an argument of a string of knowledge and adds it to the student's knowledge array
+#   #knowledge
+#     returns that student's knowledge array
---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+
+    attr_accessor :first_name, :last_name
+
+
+end
+
+# User
+#   has a first name
+#   has a last name
